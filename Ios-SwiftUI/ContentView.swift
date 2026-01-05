e7b491a98c7f318234134eaabfdf6ef7c926de06
minkyu <elliotelon@gmail.com>
2026-01-05 22:32:17 +0900

    Initial Commit

---
+++ Ios-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ios-SwiftUI
+//
+//  Created by 김민규 on 1/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ios-SwiftUI/Ios_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ios_SwiftUIApp.swift
+//  Ios-SwiftUI
+//
+//  Created by 김민규 on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Ios_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
